04c30a0eee8ed1dd24ca743e510937a788d06d86
“gabriel.alcantara” <gabriel.alcantara@g.globo>
2021-11-06 09:27:56 -0300

    Finished middleware code

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const cors = require('cors');
 
 const { v4: uuidv4, validate } = require('uuid');
+const { user } = require('./__tests__/middlewares/checksTodoExists.spec');
 
 const app = express();
 app.use(express.json());
@@ -10,19 +11,68 @@ app.use(cors());
 const users = [];
 
 function checksExistsUserAccount(request, response, next) {
-  // Complete aqui
+  const { username } = request.headers;
+
+  const user = users.find(user => user.username === username);
+
+  if(!user) {
+    return response.status(404).json({error: 'Username does not exist!'})
+  }
+
+  request.user = user;
+
+  return next();
 }
 
 function checksCreateTodosUserAvailability(request, response, next) {
-  // Complete aqui
+  const { user } = request;
+  const todosAmount = user.todos.length();
+  const canCreateTodos = user.pro || !user.pro && todosAmount <= 10 
+
+  if(!canCreateTodos) {
+    return response.status(403).json({error: 'Number of todos created exceed free plan´s limit!'})
+  }
+  
+  return next();
 }
 
 function checksTodoExists(request, response, next) {
-  // Complete aqui
+  const { id } = request.params;
+  const { username } = request.headers;
+
+  const user = users.find(user => user.username === username);
+
+  if(!user) {
+    return response.status(404).json({error: 'Username does not exist!'})
+  }
+
+  const todo = user.todos.find(todo => todo.id === id);
+  const isValidUUID = validate(id);
+
+  if(!isValidUUID) {
+    return response.status(400).json({error: 'Todo id is not a valid uuid!'});
+  }
+
+  if(!todo) {
+    return response.status(404).json({error: 'Todo does not exist for this user!'});
+  }
+  request.todo = todo;
+
+  return next();
 }
 
 function findUserById(request, response, next) {
-  // Complete aqui
+  const { id } = request.params;
+
+  const user = users.find(user => user.id === id);
+
+  if(!user) {
+    return response.status(404).json({error: `User with id ${id} does not exist!`})
+  }
+
+  request.user = user;
+
+  return next();
 }
 
 app.post('/users', (request, response) => {
